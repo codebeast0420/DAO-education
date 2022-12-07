e948317fb2eb45a504ce28383c6742882a1a06a7
John <codebeast0420@gmail.com>
2022-12-07 23:33:03 +0800

    fix bugs

--- src/layouts/sidebar/_menu-items.tsx
+++ src/layouts/sidebar/_menu-items.tsx
@@ -15,21 +15,21 @@ export const menuItems = [
     icon: <HomeIcon />,
     href: routes.home,
   },
-  {
-    name: 'Farm',
-    icon: <FarmIcon />,
-    href: routes.farms,
-  },
-  {
-    name: 'Swap',
-    icon: <ExchangeIcon />,
-    href: routes.swap,
-  },
-  {
-    name: 'Liquidity',
-    icon: <PoolIcon />,
-    href: routes.liquidity,
-  },
+  // {
+  //   name: 'Farm',
+  //   icon: <FarmIcon />,
+  //   href: routes.farms,
+  // },
+  // {
+  //   name: 'Swap',
+  //   icon: <ExchangeIcon />,
+  //   href: routes.swap,
+  // },
+  // {
+  //   name: 'Liquidity',
+  //   icon: <PoolIcon />,
+  //   href: routes.liquidity,
+  // },
   {
     name: 'Explore NFTs',
     icon: <CompassIcon />,
@@ -50,23 +50,23 @@ export const menuItems = [
     icon: <ProfileIcon />,
     href: routes.profile,
   },
-  {
-    name: 'Vote',
-    icon: <VoteIcon />,
-    href: routes.vote,
-    dropdownItems: [
-      {
-        name: 'Explore',
-        href: routes.vote,
-      },
-      {
-        name: 'Vote with pools',
-        href: routes.proposals,
-      },
-      {
-        name: 'Create proposal',
-        href: routes.createProposal,
-      },
-    ],
-  },
+  // {
+  //   name: 'Vote',
+  //   icon: <VoteIcon />,
+  //   href: routes.vote,
+  //   dropdownItems: [
+  //     {
+  //       name: 'Explore',
+  //       href: routes.vote,
+  //     },
+  //     {
+  //       name: 'Vote with pools',
+  //       href: routes.proposals,
+  //     },
+  //     {
+  //       name: 'Create proposal',
+  //       href: routes.createProposal,
+  //     },
+  //   ],
+  // },
 ];
